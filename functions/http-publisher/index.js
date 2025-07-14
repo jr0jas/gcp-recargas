146a8073983b3558b0cbfbd6fcc70736a203a673
qxz5a0x <jorge.rojas@partner.bmwgroup.com>
2025-07-13 19:44:26 -0600

    update

--- functions/http-publisher/index.js
+++ functions/http-publisher/index.js
@@ -2,6 +2,17 @@ const { PubSub } = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
 
 exports.publishRecharge = async (req, res) => {
+  // 👇 Agrega soporte para CORS
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
+
+  // 👇 Responde a solicitudes OPTIONS (preflight)
+  if (req.method === 'OPTIONS') {
+    return res.status(204).send('');
+  }
+
+  // Verifica método
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
